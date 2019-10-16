57c3d667e66fd811a393d257d25f0a89e0f7f678
Nick Lunn <nrlunn325@gmail.com>
2019-10-16 10:42:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(char = '*'){
+    return function(string = 'special'){
+        return `You are ${char}${string}${char}!`
+    }
+}
+
+let Calculator = {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => a / b
+}
+
+function actionApplyer(integer, functions){
+
+    let result = integer
+
+    for(let i = 0; i < functions.length; i++){
+        result = functions[i](result)
+    }
+
+    return result
+
+}
